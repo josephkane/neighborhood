accc6180fa968bd5a5a976ec3cc9fa9dfbfcc537
Joseph Kane <josephckane@gmail.com>
2016-09-13 13:38:50 -0500

    add gitignore, finish serializers

--- nh_rest/neighborhood/models.py
+++ nh_rest/neighborhood/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Neighborhood(models.Model):
+	name = models.CharField(max_length=20)
+
 class House(models.Model):
 	address = models.CharField(max_length=200, blank=False, null=False)
 	bed = models.IntegerField(blank=False, null=False)
@@ -8,17 +11,23 @@ class House(models.Model):
 	sq_ft = models.IntegerField(blank=False, null=False)
 	lot_size = models.IntegerField
 	yr_built = models.IntegerField
-	agent = models.ForeignKey(Agent, on_delete=SET_NULL, related_name="agent")
+	agent = models.ForeignKey(Agent, null=True, on_delete=SET_NULL, related_name="agent")
 	selling = models.BooleanField(blank=False, null=False)
-	image = models.ImageField(upload_to="../img/house_img/", default="../img/default_house.jpg")
+	image = models.ImageField(upload_to="../img/house_imgs/", default="../img/default_house.jpg")
 	neighborhood = models.ForeignKey(Neighborhood, on_delete=SET_NULL, related_name="neighborhood")
-	last_sold = models.DateTimeField(auto_add=False)
+	last_sold = models.DateField(auto_add=False)
 
-class Neighborhood(models.Model):
-	name = models.CharField(max_length=20)
 
 class Agent(User):
+	bio = models.CharField(max_length=200)
+	member_since = models.DateField(auto_now_add=True)
+	image = models.ImageField(upload_to="../img/agent_imgs/", default="../img/default_agent.jpg")
 
 class Buyer(User):
+	agent = models.ForeignKey(Agent, null=True, on_delete=SET_NULL, related_name="agent")
+	budget = models.IntegerField
+	image = models.ImageField(upload_to="../img/buyer_imgs/", default="../img/default_buyer.jpg")
 
 class Owner(User):
+	house = models.ForeignKey(House, null=True, on_delete=SET_NULL, related_name="house")
+	image = models.ImageField(upload_to="../img/owner_imgs/", default="../img/default_owner.jpg")
--- nh_rest/neighborhood/serializers.py
+++ nh_rest/neighborhood/serializers.py
@@ -1,2 +1,22 @@
 from rest_framework import serializers
-from tracks.models import House, Neighborhood,
+from tracks.models import House, Neighborhood, Agent, Buyer, Owner
+
+class NeighborhoodSerializer(serializers.HyperlinkedModelSerializer):
+	model = Neighborhood
+	fields = fields = ("id", "url", "name")
+
+class HouseSerializer(serializers.HyperlinkedModelSerializer):
+	model = House
+	fields = ("id", "url", "address", "bed", "bath", "sq_ft", "lot_size", "yr_built", "agent", "selling", "image", "neighborhood", "last_sold")
+
+class AgentSerializer(serializers.HyperlinkedModelSerializer):
+	model = Agent
+	fields = ("id", "url", "first_name", "last_name", "email", "bio", "member_since", "image")
+
+class BuyerSerializer(serializers.HyperlinkedModelSerializer):
+	model = Buyer
+	fields = ("id", "url", "first_name", "last_name", "email", "agent", "budget", "image")
+
+class OwnerSerializer(serializers.HyperlinkedModelSerializer):
+	model = Owner
+	fields = ("id", "url", "first_name", "last_name", "email", "house", "image")
