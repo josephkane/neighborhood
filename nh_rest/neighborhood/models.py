4f26a721d02446fef38262af8c62b329a96f4002
Joseph Kane <josephckane@gmail.com>
2016-09-14 10:46:51 -0500

    add agents to database

--- nh_rest/neighborhood/models.py
+++ nh_rest/neighborhood/models.py
@@ -4,10 +4,16 @@ from django.contrib.auth.models import User
 class Neighborhood(models.Model):
 	name = models.CharField(max_length=20)
 
+	def __string__(self):
+		return "{}: {}".format(self.id, self.name)
+
 class Agent(User):
 	bio = models.CharField(max_length=200)
 	member_since = models.DateField(auto_now_add=True)
 	image = models.ImageField(upload_to="../img/agent_imgs/", default="../img/default_agent.jpg")
+
+	def __string__(self):
+		return "{}: {} {}".format(self.id, self.first_name, self.last_name)
 
 class House(models.Model):
 	address = models.CharField(max_length=200, blank=False, null=False)
@@ -16,12 +22,14 @@ class House(models.Model):
 	sq_ft = models.IntegerField(blank=False, null=False)
 	lot_size = models.IntegerField
 	yr_built = models.IntegerField
-	agent = models.ForeignKey(Agent, null=True, on_delete=models.SET_NULL, related_name="house_agent")
+	house_agent = models.ForeignKey(Agent, null=True, on_delete=models.SET_NULL, related_name="house_agent")
 	selling = models.BooleanField(blank=False, null=False)
 	image = models.ImageField(upload_to="../img/house_imgs/", default="../img/default_house.jpg")
-	neighborhood = models.ForeignKey(Neighborhood, null=True, on_delete=models.SET_NULL, related_name="house_neighborhood")
+	house_neighborhood = models.ForeignKey(Neighborhood, null=True, on_delete=models.SET_NULL, related_name="house_neighborhood")
 	last_sold = models.DateField(auto_now=False)
 
+	def __string__(self):
+		return "{}: {}".format(self.id, self.address)
 
 class Buyer(User):
 	buyer_agent = models.ForeignKey(Agent, null=True, on_delete=models.SET_NULL, related_name="buyer_agent")
@@ -29,5 +37,5 @@ class Buyer(User):
 	image = models.ImageField(upload_to="../img/buyer_imgs/", default="../img/default_buyer.jpg")
 
 class Owner(User):
-	house = models.ForeignKey(House, null=True, on_delete=models.SET_NULL, related_name="owner_house")
+	owner_house = models.ForeignKey(House, null=True, on_delete=models.SET_NULL, related_name="owner_house")
 	image = models.ImageField(upload_to="../img/owner_imgs/", default="../img/default_owner.jpg")
--- nh_rest/neighborhood/serializers.py
+++ nh_rest/neighborhood/serializers.py
@@ -2,21 +2,26 @@ from rest_framework import serializers
 from neighborhood.models import House, Neighborhood, Agent, Buyer, Owner
 
 class NeighborhoodSerializer(serializers.HyperlinkedModelSerializer):
-	model = Neighborhood
-	fields = fields = ("id", "url", "name")
+	class Meta:
+		model = Neighborhood
+		fields = ("id", "url", "name")
 
 class HouseSerializer(serializers.HyperlinkedModelSerializer):
-	model = House
-	fields = ("id", "url", "address", "bed", "bath", "sq_ft", "lot_size", "yr_built", "house_agent", "selling", "image", "house_neighborhood", "last_sold")
+	class Meta:
+		model = House
+		fields = ("id", "url", "address", "bed", "bath", "sq_ft", "lot_size", "yr_built", "house_agent", "selling", "image", "house_neighborhood", "last_sold")
 
 class AgentSerializer(serializers.HyperlinkedModelSerializer):
-	model = Agent
-	fields = ("id", "url", "first_name", "last_name", "email", "bio", "member_since", "image")
+	class Meta:
+		model = Agent
+		fields = ("id", "url", "username", "password", "first_name", "last_name", "email", "bio", "member_since", "image")
 
 class BuyerSerializer(serializers.HyperlinkedModelSerializer):
-	model = Buyer
-	fields = ("id", "url", "first_name", "last_name", "email", "buyer_agent", "budget", "image")
+	class Meta:
+		model = Buyer
+		fields = ("id", "url", "username", "password", "first_name", "last_name", "email", "buyer_agent", "budget", "image")
 
 class OwnerSerializer(serializers.HyperlinkedModelSerializer):
-	model = Owner
-	fields = ("id", "url", "first_name", "last_name", "email", "owner_house", "image")
+	class Meta:
+		model = Owner
+		fields = ("id", "url", "username", "password", "first_name", "last_name", "email", "owner_house", "image")
