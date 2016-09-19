2662998a5ed2055dc64726de04e6ffe25eab1ad9
Joseph Kane <josephckane@gmail.com>
2016-09-19 09:46:04 -0500

    remove comments

--- nh_client/app/landing/landing.ctrl.js
+++ nh_client/app/landing/landing.ctrl.js
@@ -67,30 +67,5 @@ angular.module("nh")
 						.catch((err) => console.log(err))
 					})
 			}
-
-			// landCtrl.register = function () {
-			// 	console.log("register");
-
-			// 	let input = document.querySelector('[type="file"]');
-   //    			let file = input.files[0];
-   //    			console.log("IMAGE: ", file);
-
-			// 	$http({
-			// 		url: `${apiUrl}/register/`,
-			// 		method: "POST",
-			// 		headers: {"Content-type": "application/x-www-form-encoded"},
-			// 		data: {
-			// 			"username": landCtrl.userName,
-			// 			"password": landCtrl.password,
-			// 			"first_name": landCtrl.firstName,
-			// 			"last_name": landCtrl.lastName,
-			// 			"email": landCtrl.email,
-			// 			"image": file,
-			// 			"user_type": landCtrl.user_type,
-			// 			"bio": landCtrl.bio
-			// 		}
-			// 	})
-			// 	.then(() => $location.path("/profile"))
-			// 	.catch((err) => console.log(err))
-			// }
-		}])
+		}
+	])
